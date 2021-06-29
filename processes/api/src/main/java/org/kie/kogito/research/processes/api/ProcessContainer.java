a4f633635e820b30a34a3e2b47640d573816c2a4
Edoardo Vacchi <evacchi@users.noreply.github.com>
2021-06-29 11:48:02 +0200

    clean

--- processes/api/src/main/java/org/kie/kogito/research/processes/api/ProcessContainer.java
+++ processes/api/src/main/java/org/kie/kogito/research/processes/api/ProcessContainer.java
@@ -2,10 +2,6 @@ package org.kie.kogito.research.processes.api;
 
 import org.kie.kogito.research.application.api.AddressableContainer;
 
-// @Name("processes")
 public interface ProcessContainer extends AddressableContainer<Process> {
 
 }
-
-// @Template({ ProcessContainer.class, Process.class, ProcessInstanceContainer.class, ProcessInstance.class })
-class Driver {}
